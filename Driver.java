1f605af716a414f706d95fdb7bccd4c1cf9a30ba
mattstg <mattstger@gmail.com>
2013-11-15 21:21:31 -0500

    Another update!

--- Driver.java
+++ Driver.java
@@ -1,14 +1,19 @@
 package TheGreatHashtable;
 
+import TheGreatHashtable.enums.Bounds;
+import TheGreatHashtable.enums.ENode;
+
 public class Driver {
 	public static void main(String[] args) {
 		
+	/*
 		Node Node1 = new Node(1,2, null, null);
 		Node Node2 = new Node(0,1, Node1, null);
 		System.out.println("Attempting to acquire Node1's xNode:");
-		Node o = (Node) Node2.Acq(Bounds.x);
-		System.out.println("This should print the lower bound of Node1 (should be 1):" + (Node) o.Acq('l'));
+		Node o = (Node) Node2.Ret(ENode.x);
+		System.out.println("This should print the lower bound of Node1 (should be 1):" + o.Ret(Bounds.l));
 		
+	*/
 		
 	}
 }
