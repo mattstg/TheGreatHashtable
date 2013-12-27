fd0734260d9ddfe6566da7eeac30374c677be8a7
mattstg <mattstger@gmail.com>
2013-12-26 22:28:23 -0500

    Copy Cnstr

--- enums/S_Box.java
+++ enums/S_Box.java
@@ -3,13 +3,24 @@ package TheGreatHashtable.enums;
 public class S_Box {
 		 
 		//S_Box moo = new S_Box(new S_XY(),new S_XY());   how to init shortcut
+	// moo.loc.x = 5;
+	// moo.size.y = -2;
 		
+	
+	//error to look out for, becomes null out of scope, if that is case, make a copy constr 
 		public S_XY loc;
 		public S_XY size;
 
 		public S_Box(){loc = new S_XY(); size = new S_XY();}
 		public S_Box(S_XY tLoc, S_XY tSize){ loc = tLoc; size = tSize; }
+		public S_Box(int locx, int locy, int sizex, int sizey){ loc = new S_XY(locx,locy); size = new S_XY(sizex,sizey);}
 		
+		public S_Box(S_Box copyMe)
+		{
+			this.loc = new S_XY(copyMe.loc);
+			this.size = new S_XY(copyMe.size);
+			
+		}
 		
 	}
 
--- enums/S_XY.java
+++ enums/S_XY.java
@@ -2,12 +2,17 @@ package TheGreatHashtable.enums;
 
 public class S_XY {
 	
-	
+	//S_XY moo = S_XY(5,5)
+	//moo.x = 4;
 	public int x;
 	public int y;
 
 	public S_XY(){x=0;y=0;}
-	public S_XY(int tx, int ty){ x =tx; y = ty;	}
-	
+	public S_XY(int tx, int ty){ x = tx; y = ty;	}
+	public S_XY(S_XY copyMe)
+	{
+		x = copyMe.x;
+		y = copyMe.y;		
+	}
 	
 }
