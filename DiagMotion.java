705591eddecdb963fd69ce4c9284d9cc46a7a2a7
mattstg <mattstger@gmail.com>
2013-12-27 01:08:08 -0500

    kjhgkmjb

--- DiagMotion.java
+++ DiagMotion.java
@@ -1,118 +1,155 @@
 package TheGreatHashtable;
+import TheGreatHashtable.enums.*;
 
 public class DiagMotion { //diagonal motion
 		private int x,y;
-		double slope;
-		private int distx,disty = 0; //this will be the distance covered so far.
-		private Entity Et;
-		
+		private S_Box E;
+		int counter = 0;
+		int Rem;
+		double dR;
+		double CdR;
+		int t;
 	
-	//this will be the constructor we use the most. Input x velocity(a), y velocity(b), 
-	//and the Entity doing the motion to get the locations needing to be checked for collision
-	public DiagMotion(int a, int b, Entity tE){
+		//constructors
+	public DiagMotion(int a, int b, S_Box tE){
 		x = a;
 		y = b;
-		Et = new Entity(tE); //so we aren't going to have PRIVACY ISSUES AHAHAHHAHAHA
-		if(x==0||y==0){
-			if(x!=y){
-			slope = (x>y)? x : y;
-			}else{
-				slope = 0;
-			}
+		E = new S_Box(tE);
+		if(Math.abs(x)>Math.abs(y)){
+			Rem =  Math.abs(x)-Math.abs(y);
+			dR = (double) Math.abs(y)/(double)Rem;
+		}else if(Math.abs(x)<Math.abs(y)){
+			Rem = Math.abs(y)-Math.abs(x);
+			dR = (double) Math.abs(x)/(double)Rem;
 		}else{
-			slope = x/y;
+			Rem = 0;
+			dR = 0;
 		}
+		CdR = dR/2;
+		t = (Math.abs(x)>Math.abs(y)) ? Math.abs(y) : Math.abs(x);
 	}
-	
-	//default constructor will give a velocity of [1,1] to a new Entity (with size 1x1, @ (0,0))
-	//used primarily for testing.
 	public DiagMotion(){ 
 		x = 1;
 		y = 1;
-		Et = new Entity(); //see default constructor in Entity Class
-		slope = x/y;
+		E = new S_Box(); //see default constructor in Entity Class
+		Rem = 0;
+		dR = 0;
+		CdR = 0;
+		t = 1;
 	}
 	
 	public String toString(){
-		return "velocity: [" + x + " , " + y +"] & slope: " + slope;
+		return "velocity: [" + x + " , " + y +"]";
 	}
 
 	
-	//this returns two Collision Space objects which tell us the next location which we need to check...
-	public CollisionSpace[] RetColSpaces(){
+	//this returns a Box object which tell us the next location which we need to check
+	private S_Box VelToBox(){
 		int a = 0,b = 0,c = 0,d = 0;
-		CollisionSpace i[] = new CollisionSpace[2];
-		if(x>0){
-			a = 0;
-			c = Et.size[0];
-			d = c;
-		}else if(x<0){
-			a = -1; // got one case
-			d = -1;
-			c = Et.size[0]-1;
-		}else{
-			a = 0;
-			c = Et.size[0]-1 ;
-		}
-		if(y>0){
-			b = 1;
-		}else if(y<0){
-			b = (-1)*(Et.size[1]-1)-1;
-		}
-		i[0] = new CollisionSpace(Et.loc[0]+a,Et.loc[1]+b,Et.loc[0]+c,Et.loc[1]+b);
-		i[1] = new CollisionSpace(Et.loc[0]+d,Et.loc[1],Et.loc[0]+d,Et.loc[1]-(Et.size[1]-1));
-		i[1] = (x==0) ? null: i[1];
-		i[0] = (y==0) ? null: i[0];
-		if(i[1] == null){
-			//note we need to remove a bit off of the a in cases where y == 0
+		if(x<0){
+			a = -1;
+		}else if(x==0){
+			c = -1;
 		}
 		
-		System.out.println("ret overlap successful");
+		if(y>0){
+			b = 1;
+		}else if(y == 0){
+			d = -1; 
+		}
+		
+		S_Box i = new S_Box(E.loc.x+a,E.loc.y+b,E.size.x+c+1,E.size.y+d+1);
+		return i;
+	}
+	private S_Box VelToBox(int xx, int yy){
+		int a = 0,b = 0,c = 0,d = 0;
+		if(xx<0){
+			a = -1;
+		}else if(x==0){
+			c = -1;
+		}
+		
+		if(yy>0){
+			b = 1;
+		}else if(y == 0){
+			d = -1; 
+		}
+		
+		S_Box i = new S_Box(new S_XY(E.loc.x+a,E.loc.y+b),new S_XY(E.size.x+c,E.size.y+d));
 		return i;
 	}
 	
-	
 	//this method moves the copy of the object you are collision testing forward one step in its motion.
-	//must be done to get the next successive Collision Spaces.
-	public void fantomMotion(int times){
-		for(int i = times; i > 0; i--){
+	private void PMove(){	
 		int tx = 0,ty = 0; 
-		if((distx-x)!=0){
+		if(x!=0){
 			if(x>0)
 				tx = 1;
 			else
 				tx = -1;
 		}
-		if((disty-y)!=0){
+		if(y!=0){
 			if(y>0)
 				ty = 1;
 			else
 				ty = -1;
 		}	
-		Et.setLoc(Et.getLoc()[0]+tx,Et.getLoc()[1]+ty);
-		distx+=tx;
-		disty+=ty;
-		}
+		E.loc.x = E.loc.x+tx;
+		E.loc.y = E.loc.y+ty;
 	}
-	
-	public void fantomMotion(){
-		//same as above method, just without an argument.
-		int tx = 0,ty = 0;
-		if((distx-x)!=0){
+	private void PMove(int xx, int yy){	
+		int tx = 0,ty = 0; 
+		if(xx!=0){
 			if(x>0)
 				tx = 1;
 			else
 				tx = -1;
 		}
-		if((disty-y)!=0){
+		if(yy!=0){
 			if(y>0)
 				ty = 1;
 			else
 				ty = -1;
 		}	
-		Et.setLoc(Et.getLoc()[0]+tx,Et.getLoc()[1]+ty);
-		distx+=tx;
-		disty+=ty;
+		E.loc.x = E.loc.x+tx;
+		E.loc.y = E.loc.y+ty;
 	}
-	
+
+	public S_Box RetNextBox(){
+		if(counter > t){
+			return null;
+		}
+		if(counter != 0){
+			PMove();
+		}
+		S_Box i = VelToBox();
+		
+		System.out.println("counter: " + counter + "CdR: " + CdR);
+		
+		if(Rem != 0){
+			if((double)counter > CdR){
+				if(x==t){
+					if(y>0){
+						i.loc.y += 1;
+						PMove(0,1);
+					}else{
+						i.size.y += 1;
+						PMove(0,-1);
+					}
+				}else{
+					if(x>0){
+						i.size.x += 1;
+						PMove(1,0);
+					}else{
+						i.loc.x += 1;
+						PMove(-1,0);
+					}
+				}
+				CdR += dR;
+			}
+		}
+		counter++;
+		return i;
+	}
+
 }
