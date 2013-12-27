81dc7c963a3358eb34baad995d2a9e47b66058e8
mattstg <mattstger@gmail.com>
2013-12-26 23:50:56 -0500

    Hashtable n Node  New Package!
    Hashtable can now be given ethier a box to check if it colis with its
    hashTable
    Node has a toString
    New package for testDrivers

--- Hashtable.java
+++ Hashtable.java
@@ -1,8 +1,6 @@
 package TheGreatHashtable;
 
-import TheGreatHashtable.enums.Bounds;
-import TheGreatHashtable.enums.ENode;
-import TheGreatHashtable.enums.OverlapType;
+import TheGreatHashtable.enums.*;
 
 
 
@@ -20,8 +18,8 @@ public class Hashtable {
 
 	*/
 	
-public Hashtable(Object o){
-	mainNode = (Node) o;
+public Hashtable(Node hashTreeHead){
+	mainNode = hashTreeHead;
 }
 
 public Hashtable(){
@@ -62,8 +60,70 @@ private Node _pop(Node node)
 		return null;
 	
 	Node toRet = node;
-	node.Adj(node.Adj());
+	node.Adj(node.Adj()); //why did i do this?
 	return toRet;
+}
+
+private boolean Coli(Node a, Node b)
+{
+	if(RetOverlap(a,b,false) == OverlapType.After || RetOverlap(a,b,false) == OverlapType.Before)
+		return false;
+	return true;
+	
+	
+}
+
+public boolean Coli(S_Box box)
+{
+	//turn the passed box into a hashtable
+	//Node x = new Node(l,u,adj,dwn);
+	
+	Node nodeY = new Node(box.loc.y-box.size.y,box.loc.y,null,null);
+	Node nodeX = new Node(box.loc.x,box.loc.x+box.size.x,null,nodeY);
+	
+	//now have a tree for box 
+	
+	
+	return Coli(nodeX); //given the nodeX trees
+	
+	
+}
+
+
+private boolean Coli(Node hashTree)  //WORKS UNDER ASSUMPTION THAT hashTree CONTAINS 1 x and 1 y!!!!!, please grow algo if needed
+{
+	Node mainITX = mainNode;  //Iterator to main node in x plane
+	
+	while(mainITX != null)
+	{
+		//System.out.println("main" + mainITX.toString());
+		//System.out.println("hashTree" + hashTree.toString());
+		
+		
+		if(Coli(mainITX,hashTree)) //if coli in x
+		{
+			
+			//an overlap of a kind has occured, now loop through for the y laps
+			Node mainITY = mainITX.Dwn(); //retrieve the dwn node
+			
+			while(mainITY != null)
+			{
+				//System.out.println("hashTree.dwn" + hashTree.Dwn().toString());
+				//System.out.println("main.y" + mainITY.toString());
+				
+				if(Coli(mainITY,hashTree.Dwn())) //if a coli occurs between the y from local hash and y from given tree			
+					return true;//A coli has occured, this means there is a colision between these two trees
+				else				
+					mainITY = mainITY.Adj();//iterate to the next mainITY				
+			}
+			//no coli in y, go down one more x,
+			
+		}
+		mainITX = mainITX.Adj(); //iterate to next x		
+		
+	}
+	return false; //no coli occured
+	
 }
 
 
